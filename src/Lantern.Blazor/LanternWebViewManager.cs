d495b7f07c2f40907c33474dbd2604d4e991ea0a
肖昕 <xiao.xin@outlook.com>
2023-07-13 15:12:48 +0800

    23

---
+++ src/Lantern.Blazor/LanternWebViewManager.cs
@@ -0,0 +1,39 @@
+﻿using Lantern.Windows;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Extensions.FileProviders;
+
+namespace Lantern.Blazor;
+
+public class LanternWebViewManager : WebViewManager
+{
+    private readonly WebViewWindow _window;
+    public LanternWebViewManager(
+                                 WebViewWindow webviewWindow,
+                                 IServiceProvider provider,
+                                 Dispatcher dispatcher,
+                                 Uri appBaseUri,
+                                 IFileProvider fileProvider,
+                                 JSComponentConfigurationStore jsComponents,
+                                 string hostPageRelativePath) : base(provider, dispatcher, appBaseUri, fileProvider, jsComponents, hostPageRelativePath)
+    {
+        _window = webviewWindow;
+        _window.WebViewMessageReceived += OnWebViewMessageReceived;
+    }
+
+    protected override void SendMessage(string message)
+    {
+        _window.PostMessage(message);
+    }
+
+    private async void OnWebViewMessageReceived(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override async void NavigateCore(Uri absoluteUri)
+    {
+        await _window.SetUrlAsync(absoluteUri.ToString());
+    }
+}
