678d650bd6144075c7f8329d82b20b6842c732e5
肖昕 <xiao.xin@outlook.com>
2023-10-28 15:54:11 +0800

    34

---
+++ src/Lantern.Blazor/LanternDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Lantern.Blazor;
+
+public class LanternDispatcher : Dispatcher
+{
+    public override bool CheckAccess()
+    {
+        return Threading.Dispatcher.UIThread.CheckAccess();
+    }
+
+    public override Task InvokeAsync(Action workItem)
+    {
+        return Threading.Dispatcher.UIThread.InvokeAsync(workItem);
+    }
+
+    public override Task InvokeAsync(Func<Task> workItem)
+    {
+        return Threading.Dispatcher.UIThread.InvokeAsync(workItem);
+    }
+
+    public override Task<TResult> InvokeAsync<TResult>(Func<TResult> workItem)
+    {
+        return Threading.Dispatcher.UIThread.InvokeAsync(workItem);
+    }
+
+    public override Task<TResult> InvokeAsync<TResult>(Func<Task<TResult>> workItem)
+    {
+        return Threading.Dispatcher.UIThread.InvokeAsync(workItem);
+    }
+}
--- src/Lantern.Blazor/LanternWebViewManager.cs
+++ src/Lantern.Blazor/LanternWebViewManager.cs
@@ -1,4 +1,5 @@
-﻿using Lantern.Windows;
+﻿using Lantern.Platform;
+using Lantern.Windows;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebView;
@@ -6,17 +7,47 @@ using Microsoft.Extensions.FileProviders;
 
 namespace Lantern.Blazor;
 
+
+public class BlazorWebViewWindow : WebViewWindow
+{
+    private readonly IServiceProvider _serviceProvider;
+    private LanternWebViewManager? _webViewManager;
+
+    public BlazorWebViewWindow(WebViewEnvironmentOptions environmentOptions,
+                               WebViewWindowOptions windowOptions,
+                               IWindowManager? windowManager,
+                               IWindowImpl windowImpl,
+                               IServiceProvider serviceProvider) : base(environmentOptions, windowOptions, windowManager, windowImpl)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override void OnWebViewInitialized()
+    {
+        _webViewManager = new LanternWebViewManager(
+            this,
+            _serviceProvider,
+            new LanternDispatcher(),
+            null,
+            null,
+            "wwwroot/index.html");
+        //_webViewManager.Navigate(_windowOptions.Url);
+        base.OnWebViewInitialized();
+    }
+}
+
 public class LanternWebViewManager : WebViewManager
 {
+    private static readonly Uri AppBaseUri = new("https://0.0.0.0/");
+
     private readonly WebViewWindow _window;
     public LanternWebViewManager(
                                  WebViewWindow webviewWindow,
                                  IServiceProvider provider,
                                  Dispatcher dispatcher,
-                                 Uri appBaseUri,
                                  IFileProvider fileProvider,
                                  JSComponentConfigurationStore jsComponents,
-                                 string hostPageRelativePath) : base(provider, dispatcher, appBaseUri, fileProvider, jsComponents, hostPageRelativePath)
+                                 string hostPageRelativePath) : base(provider, dispatcher, AppBaseUri, fileProvider, jsComponents, hostPageRelativePath)
     {
         _window = webviewWindow;
         _window.WebViewMessageReceived += OnWebViewMessageReceived;
@@ -24,11 +55,13 @@ public class LanternWebViewManager : WebViewManager
 
     protected override void SendMessage(string message)
     {
+
         _window.PostMessage(message);
     }
 
     private async void OnWebViewMessageReceived(string message)
     {
+        this.MessageReceived();
         throw new NotImplementedException();
     }
 
