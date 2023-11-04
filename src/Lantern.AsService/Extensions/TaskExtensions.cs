fa97740fb7f59ddb41527ebca4abeeb2d91f8e2b
肖昕 <xiao.xin@outlook.com>
2023-11-04 13:09:23 +0800

    Method WithCancellation result support

--- src/Lantern.AsService/Extensions/TaskExtensions.cs
+++ src/Lantern.AsService/Extensions/TaskExtensions.cs
@@ -13,6 +13,17 @@ internal static class TaskExtensions
         await task; // already completed; propagate any exception
     }
 
+    public static async Task<TResult> WithCancellation<TResult>(this Task<TResult> task, CancellationToken cancellationToken)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
+        {
+            if (task != await Task.WhenAny(task, tcs.Task).ConfigureAwait(false))
+                throw new OperationCanceledException(cancellationToken);
+        }
+        return await task; // already completed; propagate any exception
+    }
+
     public static async Task WithCancellation(this Task task, TimeSpan timeout, CancellationToken cancellationToken)
     {
         if (timeout <= TimeSpan.Zero)
@@ -36,7 +47,29 @@ internal static class TaskExtensions
         await task; // already completed; propagate any exception
     }
 
-    public static void IgnoreException(this Task task)
+    public static async Task<TResult> WithCancellation<TResult>(this Task<TResult> task, TimeSpan timeout, CancellationToken cancellationToken)
+    {
+        if (timeout <= TimeSpan.Zero)
+        {
+            return await WithCancellation(task, cancellationToken).ConfigureAwait(false);
+        }
+
+        var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        using var cancellationTimoutSource = new CancellationTokenSource(timeout);
+        using (cancellationTimoutSource.Token.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
+        using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(false), tcs))
+        {
+            if (task != await Task.WhenAny(task, tcs.Task).ConfigureAwait(false))
+            {
+                task.IgnoreException();
+                throw tcs.Task.Result ? new TimeoutException() : new OperationCanceledException(cancellationToken);
+            }
+        }
+
+        return await task; // already completed; propagate any exception
+    }
+
+    private static void IgnoreException(this Task task)
     {
         _ = task.ContinueWith(t => t.Exception?.Handle(_ => true), CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Default);
     }
