dbe138dc1ba3af27107f98d4070635686a3deb2e
肖昕 <xiao.xin@outlook.com>
2024-02-27 09:35:26 +0100

    1

--- src/Lantern.AsService/WebViewHttpRequestExtensions.cs
+++ src/Lantern.AsService/WebViewHttpRequestExtensions.cs
@@ -7,4 +7,11 @@ public static class WebViewHttpRequestExtensions
         request.Headers.TryGetValue("user-agent", out string? userAgent);
         return userAgent;
     }
+
+    public static string? GetCookie(this WebViewHttpRequest request)
+    {
+        request.Headers.TryGetValue("cookie", out string? cookie);
+        return cookie;
+    }
+
 }
