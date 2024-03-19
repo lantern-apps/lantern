7c5c0e2e86a55acf26c963006911a94ad4f2f199
肖昕 <xiao.xin@outlook.com>
2024-03-19 20:00:00 +0800

    1

--- src/Lantern.AsService/WebViewBrowserManager.cs
+++ src/Lantern.AsService/WebViewBrowserManager.cs
@@ -5,22 +5,12 @@ using Microsoft.Extensions.Logging;
 
 namespace Lantern.AsService;
 
-public class WebBrowserManager : IWebBrowserManager, IDisposable
+public sealed class WebBrowserManager(
+    WebViewEnvironmentOptions environmentOptions,
+    ILoggerFactory loggerFactory,
+    IWindowingPlatform windowingPlatform) : IWebBrowserManager, IDisposable
 {
-    private readonly List<WebBrowserWindow> _windows = new();
-    private readonly ILoggerFactory _loggerFactory;
-    private readonly IWindowingPlatform _windowingPlatform;
-    private readonly WebViewEnvironmentOptions _environmentOptions;
-
-    public WebBrowserManager(
-        WebViewEnvironmentOptions environmentOptions,
-        ILoggerFactory loggerFactory,
-        IWindowingPlatform windowingPlatform)
-    {
-        _loggerFactory = loggerFactory;
-        _windowingPlatform = windowingPlatform;
-        _environmentOptions = environmentOptions;
-    }
+    private readonly List<WebBrowserWindow> _windows = [];
 
     public WebViewBrowser? Get(string name)
     {
@@ -45,15 +35,15 @@ public class WebBrowserManager : IWebBrowserManager, IDisposable
         return window.Browser!;
     }
 
-    protected virtual WebBrowserWindow CreateWindowCore(WebViewWindowOptions windowOptions)
+    private WebBrowserWindow CreateWindowCore(WebViewWindowOptions windowOptions)
     {
         windowOptions.Name ??= $"Lantern-Window-{Guid.NewGuid():N}";
 
-        var logger = _loggerFactory.CreateLogger<WebBrowserWindow>();
+        var logger = loggerFactory.CreateLogger<WebBrowserWindow>();
         var window = new WebBrowserWindow(
-            _environmentOptions,
+            environmentOptions,
             windowOptions,
-            _windowingPlatform.CreateWindow(),
+            windowingPlatform.CreateWindow(),
             logger);
 
         window.Closed += () => _windows.Remove(window);
